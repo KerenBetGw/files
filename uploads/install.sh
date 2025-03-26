6b53f0a97fe50ec04b77b1fe88ce7db55d5cc14b
KerenBetGw <ryu.paris05@gmail.com>
2025-03-26 10:15:47 +0700

    Memperbarui install.sh

--- uploads/install.sh
+++ uploads/install.sh
@@ -4,7 +4,7 @@ sudo curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 sudo apt install unzip jq -y
-sudo wget -O /root/mora.zip https://raw.githubusercontent.com/KerenBetGw/files/main/uploads/mora.zip
+sudo wget -O /root/mora.zip https://github.com/KerenBetGw/files/releases/latest/download/mora_2025-03-26_10-15-32.zip
 sudo unzip /root/mora.zip -d /root/mora/
 sudo rm /root/mora.zip
 sudo chmod +x /root/mora/start.sh
