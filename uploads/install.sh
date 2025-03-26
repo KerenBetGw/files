a1d8d7fb7deb940299ed9ec491ac45023ecf3715
KerenBetGw <ryu.paris05@gmail.com>
2025-03-26 10:05:55 +0700

    Menambahkan install.sh

---
+++ uploads/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y
+sudo curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+sudo apt install unzip jq -y
+sudo wget -O /root/mora.zip https://raw.githubusercontent.com/KerenBetGw/files/main/uploads/mora.zip
+sudo unzip /root/mora.zip -d /root/mora/
+sudo rm /root/mora.zip
+sudo chmod +x /root/mora/start.sh
+sudo screen -dmS mora /root/mora/start.sh
